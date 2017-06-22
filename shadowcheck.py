302ea798197920294902aad2a1f26c22965a5d7b
Oliver Heering <heering@riege.com>
2017-06-22 07:32:04 +0200

    Revert "New "Common Pokemon""
    This reverts commit c3c490d09980f1c2f81636e23b1ed1675bd0a415.

--- shadowcheck.py
+++ shadowcheck.py
@@ -30,25 +30,21 @@ COMMON_POKEMON = [
     27,     # Sandshrew
     29,     # Nidoran F
     32,     # Nidoran M
-    37,     # Vulpix
     41,     # Zubat
     43,     # Oddish
     46,     # Paras
     52,     # Meowth
     54,     # Psyduck
-    58,     # Growlithe
     60,     # Poliwag
     69,     # Bellsprout
     72,     # Tentacool
     74,     # Geodude
     77,     # Ponyta
     81,     # Magnemite
-    90,     # Shellder
     98,     # Krabby
     118,    # Goldeen
     120,    # Staryu
     129,    # Magikarp
-    155,    # Cyndaquil
     161,    # Sentret
     165,    # Ledyba
     167,    # Spinarak
@@ -59,9 +55,7 @@ COMMON_POKEMON = [
     194,    # Wooper
     198,    # Murkrow
     209,    # Snubbull
-    218,    # Slugma
-    220,    # Swinub
-    228     # Houndour
+    218     # Slugma
 ]
 
 acc_stats = {
